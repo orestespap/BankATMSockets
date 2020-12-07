97dbc2b3ddab7bbdc5f01aa627385f137689e4ff
Orestes Papanastassiou <orestpap@gmail.com>
2020-12-08 01:14:12 +0200

    minor changes

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -9,7 +9,7 @@ public class Database {
     private static MongoCollection<Document> clientsCollection;
 
     public static void connectToDatabase(){
-        MongoClientURI uri = new MongoClientURI("mongodb://atm:#1821_1951!1997@145.14.157.133:27017/?authSource=admin");
+        MongoClientURI uri = new MongoClientURI("mongodb://");
         MongoClient mongoClient = new MongoClient(uri);
         MongoDatabase bankDB = mongoClient.getDatabase("bankDB");
         clientsCollection = bankDB.getCollection("clients");
@@ -20,7 +20,4 @@ public class Database {
         return clientsCollection;
     }
 
-    public static void getWithdrawalLock(){
-
-    }
 }
