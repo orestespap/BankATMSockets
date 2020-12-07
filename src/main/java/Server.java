a44cb6678fed53a8ff6a42aef3ca8aa06c8116c3
Orestes Papanastassiou <orestpap@gmail.com>
2020-12-07 02:27:08 +0200

    Testing

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -28,7 +28,7 @@ public class Server {
 
             ServerThread aWorker = new ServerThread(anActiveSocket);
             aWorker.start();
-            if (1==3)
+            if (1==4)
                 break;
         }
         System.out.println("Server is shutting down gracefully ...");
